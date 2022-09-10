d662dc0b17d9f7aca390fdb643551834ec7258ca
Nowkshi <61665553+Ikrachowdhury@users.noreply.github.com>
2022-09-10 14:14:13 +0600

    comment added to feed

--- feed.php
+++ feed.php
@@ -1,4 +1,4 @@
-<section style="background-color: #eee;">
+<section >
     <div class="container my-2 py-5 px-2">
         <div class="row d-flex justify-content-center">
             <div class="col-md-12 col-lg-10 col-xl-8">
@@ -26,13 +26,42 @@
                         </div>
                     </div>
 
+                    <div class="row px-3">
+                        <div class="col-sm-10 mb-2 mt-2">
+                            <input type="text" class="form-control" placeholder="Write Comment" style="font-size:12px ;">
+                        </div>
+                        <div class="col-sm-2">
+                            <a href="" class="btn btn-sm btn-primary mb-2 mt-2">Comment</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <!-- <h6 class="mb-0">Recent comments</h6>
+                        <p class="fw-light mb-4 pb-2">Latest Comments section by users</p> -->
+                        <div class="d-flex flex-start">
+                            <img class="rounded-circle shadow-1-strong me-3" src="images/club.png" alt="avatar" width="30" height="30" />
+                            <div class="pe-2">
+                                <!-- <h6 class="fw-bold mb-1">Maggie Marsh</h6> -->
+                                <div class="d-flex align-items-center mb-1" style="justify-content: space-between;">
+                                    <h6 class="fw-bold mb-1">Maggie Marsh</h6>
+                                    <p class="mb-0">
+                                        <small class="text-muted">March 07, 2021</small>
+                                    </p>
+                                </div>
+                                <p class="mb-0 " style="text-align:justify;">
+                                    <small>Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        industry. Lorem Ipsum has been the industry's standard dummy text ever
+                                        since the 1500s, when an unknown printer took a galley of type and
+                                        scrambled it.</small>
+                                </p>
+                            </div>
+                        </div>
 
 
-
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
     </div>
 </section>
