58690e6e8ea220453665563a405dc7724e9742cc
Nowkshi <61665553+Ikrachowdhury@users.noreply.github.com>
2022-09-18 00:17:52 +0600

    changed nav

--- admin/admineditclub.php
+++ admin/admineditclub.php
@@ -10,25 +10,8 @@
  ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"> -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="../css//user.css">
-    <link rel="stylesheet" href="../css//nav.css">
-    <link rel="stylesheet" href="../css//side.css"> 
-    <link rel="stylesheet" href="../css//discover.css"> 
-    <link rel="stylesheet" href="../css//pr.css">
-    <link rel="stylesheet" href="../css//profile.css"> 
-    <link rel="stylesheet" href="../css//tableCss/table.css">
-    <link rel="stylesheet" href="../css//tableCss/editclub.css">
-
-
-
+     <!-- ------------------------------------------all link and css-------------------------------------------- -->
+     <?php include '../assets//linkheader.php';?>   
      
 <title>user Structure</title>
 </head>
@@ -36,18 +19,11 @@
 <div class="container-scroller">
 <!-------------------------------- header and sidebar---------------------- -->
 <div class="header"> 
-<?php include '../assets//membernav.php';?> 
+<?php include '../assets//nav.php';?> 
 </div>
 <!---------------------------------------------totall page-------------------------------------------- -->
  
-<div class="maindiv"> 
-    
-    
-<!-- ----------------------------------------sidebar area fix------------> 
-<nav class="sidebar col-xl-3 "> 
-<?php include '../assets//side.php';?> 
-
-</nav> 
+<div class="maindiv">  
 <!--------------------------------------- Discover page body---- -->
 <div class="mainsection"> 
 
--- admin/adminmanageapplicant.php
+++ admin/adminmanageapplicant.php
@@ -2,7 +2,7 @@
  
  session_start();
  $user_id=$_SESSION['user_id']; 
- $club_id =$_SESSION['club_id'];
+ $club_id = $_SESSION['club_id'];
  $club_name = $_SESSION['club_name']; 
  $_SESSION['in_club']=1;
   
@@ -10,57 +10,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+     <!-- ------------------------------------------all link and css-------------------------------------------- -->
+     <?php include '../assets//linkheader.php';?>   
 
-    <link rel="stylesheet" href="../css//user.css">
-    <link rel="stylesheet" href="../css//nav.css">
-    <link rel="stylesheet" href="../css//side.css">  
-    <link rel="stylesheet" href="../css//pr.css">
-    <link rel="stylesheet" href="../css//profile.css">
-    <link rel="stylesheet" href="../css//register.css">
-    <link rel="stylesheet" href="../css//tableCss//applicant.css">
-
-
-
-
-    
-    
-<title>user Structure</title>
+    <title>user Structure</title>
 </head>
 <body> 
 <div class="container-scroller">
 <!-------------------------------- header and sidebar---------------------- -->
 <div class="header"> 
-<?php include '../assets//membernav.php';?> 
+<?php include '../assets//nav.php';?>  
 </div>
 <!---------------------------------------------totall page-------------------------------------------- -->
  
-<div class="maindiv"> 
-    
-    
-<!-- ----------------------------------------sidebar area fix------------> 
-<nav class="sidebar col-xl-3 "> 
-<?php include '../assets//side.php';?> 
-
-</nav> 
+<div class="maindiv">  
 <!--------------------------------------- Discover page body---- -->
-<div class="mainsection"> 
-
-<?php include 'manageapplicant.php'; ?>
-
-<!-- manage mamber deisgn -->
+<div class="mainsection">  
+ 
+    <div><?php include 'manageapplicant.php'; ?></div> 
  
 
-
-
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </div>  
 </body>
 </html>
