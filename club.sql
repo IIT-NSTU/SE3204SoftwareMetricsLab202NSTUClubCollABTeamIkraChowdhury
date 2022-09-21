153f7145ba8e1e30091ad9246a91006b044aa0e4
Nowkshi <61665553+Ikrachowdhury@users.noreply.github.com>
2022-09-22 01:05:32 +0600

    final

--- club.sql
+++ club.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2022 at 08:25 PM
+-- Generation Time: Sep 21, 2022 at 11:48 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -90,16 +90,16 @@ CREATE TABLE `clubs` (
 --
 
 INSERT INTO `clubs` (`club_id`, `club_name`, `totall_members`, `club_type`, `club_description`, `club_image`) VALUES
-(28, 'NSTU Adventure Club', 33, 'Adventure', 'Presenting the Noakhali Science & Technology University Adventure Club, a place to share joy,adventure,happiness. Beyond all these, it\'s about celebrating immense wilderness, to learn and discuss skills to make your outdoor trips safer and more pleasant.', 'adventureClub.jpg'),
-(33, 'NSTU Debating Society', 74, 'Educational', '\r\nNSTU Debating Society is working to spread the intoxication of reasoning and thinking among the students who come to study in this university from different parts of the country.\r\nThis group is to bring everyone together in its new journey, where Nobiprobe debaters will connect with everyone across the country.', '296079530_969689971094265_4244960714165675887_n.png'),
+(28, 'NSTU Adventure Club', 32, 'Adventure', 'Presenting the Noakhali Science & Technology University Adventure Club, a place to share joy,adventure,happiness. Beyond all these, it\'s about celebrating immense wilderness, to learn and discuss skills to make your outdoor trips safer and more pleasant.', 'adventureClub.jpg'),
+(33, 'NSTU Debating Society', 71, 'Educational', 'à¦¦à§‡à¦¶à§‡à¦° à¦¨à¦¾à¦¨à¦¾ à¦ªà§à¦°à¦¾à¦¨à§à¦¤ à¦¥à§‡à¦•à§‡ à¦à¦‡ à¦¬à¦¿à¦¶à§à¦¬à¦¬à¦¿à¦¦à§à¦¯à¦¾à¦²à§Ÿà§‡ à¦ªà§œà¦¤à§‡ à¦†à¦¸à¦¾ à¦¶à¦¿à¦•à§à¦·à¦¾à¦°à§à¦¥à§€à¦¦à§‡à¦° à¦®à¦¾à¦à§‡ à¦¯à§à¦•à§à¦¤à¦¿ à¦“ à¦®à¦¨à¦¨à§‡à¦° à¦¨à§‡à¦¶à¦¾ à¦›à§œà¦¿à§Ÿà§‡ à¦¦à¦¿à¦¤à§‡ à¦•à¦¾à¦œ à¦•à¦°à§‡ à¦¯à¦¾à¦šà§à¦›à§‡ à¦¨à§‹à¦¬à¦¿à¦ªà§à¦°à¦¬à¦¿ à¦¡à¦¿à¦¬à§‡à¦Ÿà¦¿à¦‚ à¦¸à§‹à¦¸à¦¾à¦‡à¦Ÿà¦¿à¥¤\r\nà¦¨à¦¤à§à¦¨ à¦•à¦°à§‡ à¦à¦° à¦ªà¦¥à¦šà¦²à¦¾à§Ÿ à¦¸à¦¬à¦¾à¦‡à¦•à§‡ à¦à¦•à¦¤à§à¦°à¦¿à¦¤ à¦•à¦°à¦¤à§‡à¦‡ à¦à¦‡ à¦—à§à¦°à§à¦ª,à¦¯à§‡à¦–à¦¾à¦¨à§‡ à¦¸à¦¾à¦°à¦¾à¦¦à§‡à¦¶à§‡à¦° à¦¸à¦¬à¦¾à¦° à¦¸à¦¾à¦¥à§‡à¦‡ à¦¸à¦‚à¦¯à§‹à¦— à¦˜à¦Ÿà¦¬à§‡ à¦¨à§‹à¦¬à¦¿à¦ªà§à¦°à¦¬à¦¿à¦° à¦¬à¦¿à¦¤à¦¾à¦°à§à¦•à¦¿à¦•à¦¦à§‡à¦°à¥¤', '296079530_969689971094265_4244960714165675887_n.png'),
 (34, 'IT Club', 35, 'Educational', 'IT Club provides students with opportunities to discuss various IT issues outside the classroom.', '299143166_473534951148908_1695854776916005098_n.jpg'),
-(36, 'Chitrokrit', 8, 'Cultural', 'Chitrokrit is an exhibition page of interdisciplinary visual arts. It is and will be solely dedicated to providing a platform and promoting visual arts by students of Noakhali Science and Technology University(NSTU)', '297429773_638637490943613_1378105625457766996_n.jpg'),
-(37, 'NSTU Photography Club', 5, 'Cultural', 'Noakhali Science and Technology University has sea-alike possibilities and NSTUPC is a small part of that sea and a field for expressing the talent of emerging photographers of NSTU.', '301840525_825842268598036_7911772817667630618_n.jpg'),
-(38, 'NSTU Business Club', 34, 'Educational', 'NSTU Business Club strives to increase awareness in the arts, media and entertainment arenas through education and professional opportunities, network-building with alumni and industry representatives, and showcasing the creative talent and artistic diversity within the NSTUBC community', '259808044_439237467573013_5664132199158856628_n.jpg'),
-(39, 'NSTU Dance Club', 9, 'Cultural', 'NSTU dance club is a cultural club of Noakhali Science and Technology University whose main motto is to assemble dancers and students willing to learn dance in a platform where they can share their skill and beauty of dance.', '277821499_339740154797150_5045825695064316361_n.jpg'),
-(40, 'NSTU Model United Nations Association', 8, 'Cultural', 'This NSTUMUNA family is for all general students of NSTU. Its two main objectives are Skilling ourselves (students) a little more. And representing our university in various universities.', '306051318_1273688043429287_2111994020083436662_n.png'),
-(42, 'Dhrupod', 84, 'cultural', 'Dhrupod is a Campus-based music club of Noakhali Science and Technology University.', 'dhrupod.jpg'),
-(43, 'Royal Economics Club', 83, 'Educational', 'Royal Economics Club (REC) is a students led the organization of the department of economics, Noakhali Science And Technology University which aims to make the students more skillful in both curricular and extracurricular activities.', '297039107_2298349560323081_8141469850951502757_n.jpg');
+(36, 'Chitrokrit', 5, 'Cultural', 'à¦šà¦¿à¦¤à§à¦°à¦•à§ƒà§Ž(Chitrokrit) is an exhibition page of interdisciplinary visual arts. It is and will be solely dedicated to providing a platform and promoting visual arts by students of Noakhali Science and Technology University(NSTU).', '297429773_638637490943613_1378105625457766996_n.jpg'),
+(37, 'NSTU Photography Club', 1, 'Cultural', 'Noakhali Science and Technology University has sea-alike possibilities and NSTUPC is a small part of that sea and a field for expressing the talent of emerging photographers of NSTU.', '301840525_825842268598036_7911772817667630618_n.jpg'),
+(38, 'NSTU Business Club', 30, 'Educational', 'NSTU Business Club strives to increase awareness in the arts, media and entertainment arenas through education and professional opportunities, network-building with alumni and industry representatives, and showcasing the creative talent and artistic diversity within the NSTUBC community', '259808044_439237467573013_5664132199158856628_n.jpg'),
+(39, 'NSTU Dance Club', 5, 'Cultural', 'NSTU dance club is a cultural club of Noakhali Science and Technology University whose main motto is to assemble dancers and students willing to learn dance in a platform where they can share their skill and beauty of dance.', '277821499_339740154797150_5045825695064316361_n.jpg'),
+(40, 'NSTU Model United Nations Association', 5, 'Cultural', 'This NSTUMUNA family is for all general students of NSTU. Its two main objectives are Skilling ourselves (students) a little more. And representing our university in various universities.', '306051318_1273688043429287_2111994020083436662_n.png'),
+(42, 'Dhrupod', 80, 'cultural', 'Dhrupod is a Campus-based music club of Noakhali Science and Technology University.', 'dhrupod.jpg'),
+(43, 'Royal Economics Club', 80, 'Educational', 'Royal Economics Club (REC) is a students led the organization of the department of economics, Noakhali Science And Technology University which aims to make the students more skillful in both curricular and extracurricular activities.', '297039107_2298349560323081_8141469850951502757_n.jpg');
 
 -- --------------------------------------------------------
 
@@ -119,13 +119,9 @@ CREATE TABLE `club_members` (
 
 INSERT INTO `club_members` (`user_id`, `club_id`, `member_type`) VALUES
 (1, 34, 'member'),
-(1, 36, 'member'),
-(1, 39, 'member'),
 (4, 28, 'member'),
 (4, 33, 'member'),
 (4, 34, 'member'),
-(4, 38, 'member'),
-(4, 43, 'member'),
 (5, 28, 'admin'),
 (5, 33, 'admin'),
 (5, 34, 'admin'),
@@ -138,54 +134,29 @@ INSERT INTO `club_members` (`user_id`, `club_id`, `member_type`) VALUES
 (5, 43, 'admin'),
 (35, 28, 'member'),
 (35, 34, 'admin'),
-(35, 36, 'member'),
-(35, 37, 'member'),
-(35, 42, 'member'),
 (36, 34, 'member'),
 (38, 34, 'member'),
-(38, 40, 'member'),
 (39, 34, 'member'),
-(39, 40, 'member'),
 (40, 34, 'admin'),
-(40, 38, 'member'),
 (41, 34, 'member'),
 (42, 34, 'member'),
-(43, 33, 'member'),
 (43, 34, 'member'),
-(43, 38, 'member'),
-(43, 39, 'member'),
-(43, 43, 'member'),
 (44, 34, 'member'),
-(44, 37, 'member'),
 (45, 34, 'member'),
-(45, 39, 'admin'),
 (46, 34, 'member'),
-(46, 39, 'member'),
-(46, 40, 'member'),
-(46, 42, 'member'),
 (47, 34, 'member'),
-(48, 28, 'member'),
 (48, 34, 'member'),
-(48, 37, 'member'),
 (49, 34, 'member'),
 (50, 34, 'member'),
-(51, 33, 'member'),
 (51, 34, 'member'),
-(51, 36, 'member'),
 (52, 34, 'member'),
 (53, 34, 'member'),
-(53, 38, 'member'),
 (54, 34, 'admin'),
-(54, 37, 'member'),
-(54, 42, 'member'),
 (55, 34, 'member'),
 (56, 34, 'member'),
-(56, 43, 'member'),
-(57, 33, 'member'),
 (57, 34, 'member'),
 (58, 34, 'member'),
 (59, 34, 'member'),
-(59, 42, 'member'),
 (60, 34, 'member'),
 (61, 34, 'member'),
 (62, 34, 'member'),
@@ -210,9 +181,10 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`comment_id`, `user_id`, `post_id`, `comment_content`, `comment_time`) VALUES
+(3, 5, 56, 'Hope to join in the session.', '09.21.2022'),
+(4, 35, 56, 'Hello', '09.21.2022'),
 (5, 63, 50, 'whaaaaaaaaaaaaaaaaaaat this is my picture???', '09.21.2022'),
-(6, 35, 57, 'Congratulations.', '09.21.2022'),
-(7, 5, 59, 'Gona join', '09.21.2022');
+(6, 35, 57, 'Congratulations.', '09.21.2022');
 
 -- --------------------------------------------------------
 
@@ -387,12 +359,16 @@ CREATE TABLE `post` (
 
 INSERT INTO `post` (`post_id`, `user_id`, `club_id`, `post_caption`, `post_picture`, `post_status`, `post_time`) VALUES
 (20, 1, 27, 'Hello from drhupod', 'joined.png', 'private', '9-10-22'),
+(26, 1, 27, 'new post dhrupod', '1_CDCeyYxNhl-VTM0-MtmC-Q.png', 'Public', '09.14.2022'),
+(27, 4, 28, 'arman posted to advanture club 4,28', 'img.png', 'Public', '09.14.2022'),
+(32, 4, 30, 'klhgjfdg', 'mess.png', 'Public', '09.18.2022'),
 (34, 1, 28, '\r\nবিলাইছড়ির ধূপপানি ঝর্ণা এই মুহূর্তে বাংলাদেশের সবচেয়ে সুন্দর ঝর্ণাগুলোর মধ্যে একটি। সুবিশাল উচ্চতা, শুভ্র জলরাশির ঝর্ণা আর ঝর্ণার নিচের গুহার জন্য ট্রেকারদের কাছে এই ঝর্নাটির আবেদন সবসময় অন্যরকম। ঝর্ণার স্বচ্ছ পানি এবং অনেক উচুঁ থেকে আছড়ে পড়া জলরাশি আপনাকে একরকম পাগল করে দিবে, ধূপপানি ঝর্ণার নিচের গুহায় চোখ বন্ধ করে বসলে মনে হবে অন্য কোন জগতে চলে গেছেন। \r\nটুকে রাখুন ১৬-১৮ সেপ্টেম্বরের দিনগুলো আর যুক্ত হয়ে যান আমাদের সাথে \"বিলাইছড়ি : লাল পাহাড়ির দেশে\" ইভেন্টে।', '304738982_762403141492679_7090714083179021838_n.jpg', 'Public', '09.18.2022'),
 (35, 1, 28, '\"ঝর্ণার খোজেঁ বোয়ালিয়ায়\"\r\nসকাল সাড়ে সাতটায় সোনাপুর জিরো পয়েন্ট থেকে যাত্রাশুরু, গন্তব্য মিরসরাই বোয়ালিয়া ট্রেইল। বাসে সমবেত সুরে গান উপভোগ করতে করতে কখন যে মিরসরাই পৌঁছালাম, টেরই পেলাম না। ঘড়িতে তখন সাড়ে দশটা, স্থানীয় একটি হোটেলে ব্যাগ রেখে আমরা রওনা দিলাম ট্রেইলের উদ্দ্যেশ্যে। বোয়ালিয়া ট্রেইলটিতে দুইটি অংশ আছে। ট্রেইলটির সুন্দরতম স্থান বোয়ালিয়া ঝর্ণার দেখা মিলবে প্রথম অংশে। দ্বিতীয় অংশে আছে অসম্ভব সুন্দর ঝিরিপথ, অমরমানিক্য ঝর্না, ন হাইত্যে খুম সহ আরো অনেক ছোটো ছোটো ঝর্না। \r\nমনের আনন্দে সবাই দ্রুত এগুতে থাকলো। ঘন্টাখানেক  হাঁটার পর, গলাসমান পানি অতিক্রম করে দেখা মিললো সুন্দরতম স্থান বোয়ালিয়া ঝর্ণা। স্রষ্টার কি অপরুপ সৃষ্টি! কি অপরুপ নির্ঝর! ঝর্নার শীতল পানি সবাই খুব উপভোগ করছিলো। ক্ষনিকের জন্য মনে হলো বাড়িটা এখানে হলে তিনবেলা শীতল এ পানিতে স্নান করতে পারলে মন্দ হতো না। এ ঝর্নার একটি বিশেষত্ব হল, ঝর্নার ভেতরে একটি ফাকা জায়গা আছে অনেকটা ছোটো ডোবার মতো। উপর থেকে পানি এসে এখানে জমা হচ্ছে। ঘন্টাখানেক সময় এখানেই কাটলো। আমরা কয়েকজন আগে আগে রওনা দিলাম, গন্তব্য ট্রেইলের দ্বিতীয় অংশ। সবার আগে সৌন্দর্য উপভোগ করতে ভালোই লাগে। ঝিরিপথ ধরে যতোই সামনে এগোচ্ছি ততোই মুগ্ধ হচ্ছি। চারদিক ঘন জঙ্গলে ঘেরা, উচুঁ নিচু আঁকাবাঁকা পথ কিংবা পিচ্ছিল পাথর কি নেই এখানে! এতো সুন্দর উপভোগ্য প্রকৃতিতে ক্লান্তি আসার সুযোগ নাই। শীতল পানিতে নিমিষেই দূর হয়ে যাবে যেকোনো ক্লান্তি। দূর্গম ঝিরিপথ দেখে মাঝে মাঝে ভয় ও লাগছিলো, তাতেও সৌন্দর্যের কোনো কমতি নেই। তাজ্জব হয়ে আপনিও বলবেন এ যে ভয়ংকর সুন্দর! মাঝপথে শুরু হলো গানের আড্ডা। গুণি মানুষজনের সাথে ভ্রমন বৃথা যায় না। শীতল মনোমুগ্ধকর পরিবেশ সাথে সুমিষ্ট সুরে কোরাস গান পুরো ভ্রমণে একটি ভিন্ন মাত্রা যুক্ত করেছে। এ ঝিরিপথের যেনো কোনো অন্ত নেই। কিন্তু তিন-তিনটি আছাড়ে কুপকাত হয়ে আর সামনে এগোনোর সাহস পেলাম না। লম্বা সময় হাটার পর সবাই হোটেলে ফিরলাম। খাওয়া দাওয়া শেষে কিছুক্ষন বিশ্রাম অতঃপর বাসে উঠে পড়লাম। ক্লান্ত শরীরে নিমিষেই ঘুমিয়ে পড়লাম। প্রায় চল্লিশজনের গ্রুপ ছিলাম আমরা, মানুষগুলো ছিলো অনবদ্য সাথে সুন্দর গুছানো আয়োজন। আত্মার প্রশান্তির জন্য এমন আয়োজন বারংবার চাই। আজ এখানেই ইতি টানছি। কথা হবে অন্যকোনো ভ্রমণের পর।', '300175106_1453990828410103_815832942459634415_n.jpg', 'Public', '09.18.2022'),
 (49, 5, 34, 'Hello programmers! International Programmers\' Day is quickly approaching, and we are happy to let you know about it. IT CLUB NSTU has decided to celebrate \'World Programmers\' Day\' on 13 September 2022 through organizing a carnival at 2rd floor, Library Building, NSTU from 10.00 AM to 4 PM.', '306476526_568504798402451_1128823782142639016_n.jpg', 'Public', '09.20.2022'),
 (50, 5, 36, 'Neymar da Silva Santos JÃºnior, \r\nThe Perfect Chaos', '307089410_3344537475802226_8525425874153245127_n.jpg', 'Public', '09.20.2022'),
 (51, 5, 37, 'Hurry up. Recruitment is going on.', '305923957_3276775829257452_5063546480919895529_n.jpg', 'Public', '09.20.2022'),
 (53, 5, 39, 'NSTU dance club is a cultural club of Noakhali Science and Technology University whose main motto is to assemble dancers and students willing to learn dance in a platform where they can share their skill and beauty of dance.', '295290888_5363975273688373_6598486535871650067_n.jpg', 'Public', '09.20.2022'),
+(56, 5, 43, 'Royal Economics Club and Bangladesh Youth Leadership Center (BYLC), BYLCx presents \r\nBlended Workshop on Kick Start Your Career.', '307657349_1533839287066762_607429440969716104_n.jpg', 'Public', '09.20.2022'),
 (57, 5, 34, 'Intra Department Programming Contest has been held on September 13,2022.', '306845937_570119771574287_9029030675056616940_n.jpg', 'Public', '09.21.2022'),
 (59, 35, 34, 'Hurry up! Registration is going on for International Programmers Day Carnival.', '306476526_568504798402451_1128823782142639016_n.jpg', 'Public', '09.21.2022'),
 (60, 40, 34, 'Welcome Prosanto! Prosanto Deb is a new admin of the club. ', '301535541_3269657169990777_4516925668432245305_n.jpg', 'Private', '09.21.2022');
@@ -436,7 +412,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `user_image`, `user_type`, `department`, `batch`, `is_validate`, `v_code`) VALUES
-(1, 'Ikra chy nowkshi', 'ikra2514@student.nstu.edu.bd', '1ff1de774005f8da13f42943881c655f', 'IMG-20211231-WA0157.jpg', 'Student', 'IIT', 14, 1, 'fe6ff2f3'),
+(1, 'Ikra chy nowkshi', 'ikra2514@student.nstu.edu.bd', '1ff1de774005f8da13f42943881c655f', 'IMG-20211231-WA0157.jpg', 'Student', 'IIT', 1, 1, 'fe6ff2f3'),
 (4, 'Armanur Rashid ', 'arman@student.nstu.edu.bd', '37693cfc748049e45d87b8c7d8b9aacd', 'IMG-20211126-WA0001.jpg', 'student', 'IIT', 14, 1, ''),
 (5, 'Sanjida NItu', 'nitu@student.nstu.edu.bd', 'd89104497e5d0ce69d9df5ef49cfd9d7', 'IMG-20211216-WA0013.jpg', 'student', 'IIT', 14, 1, ''),
 (35, 'Shahriar Ahmed', 'shahriar2514@student.nstu.edu.bd', '32c8ddfbd374f1499f1a8ec5382f2234', 'Siyam1.jpg', 'Student', 'IIT', 14, 1, '34f1a9fa'),
@@ -576,7 +552,7 @@ ALTER TABLE `clubs`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `comment_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `comment_reply`
